514c33ba0e7f27c38553629043ec584b92af889e
Laís Matie <lmatiehara@gmail.com>
2020-03-17 22:47:47 -0400

    Extraindo dados da planilha

--- Aula02/src/model/dao/ArrecadacaoDAO.js
+++ Aula02/src/model/dao/ArrecadacaoDAO.js
@@ -1,26 +1,16 @@
 import fs from 'fs'; //le e grava o arquivo File System
-import path from 'path'//aponta onde está o arquivo
+import path from 'path';//aponta onde está o arquivo
 import xlsToJson from 'convert-excel-to-json';
 
 import Municipio from '../entity/Municipio';
+import {ANOS} from '../../constants/Colunas';
 
 export default class ArrecadacaoDAO{
 
     carregarArrecadacoesDosMunicipios(){
         let municipios = [];
 
-        /**
-         * Aponto para o arquivo 
-         * da planilha de dados,
-         * a partir da pasta atual
-         * caminho relativo, não absoluto
-         */
-        let caminhoArquivo = path.resolve(__dirname,'../../data/dados.xls');
-         
-        let dados = xlsToJson({
-            source: fs.readFileSync(caminhoArquivo),
-            range: 'A4:P185'
-        });
+        let dados = this.extrairDadosDaPlanilha('A4:P185');
 
         dados.Plan1.forEach(elemento =>{
             let nome = elemento.A;
@@ -36,5 +26,33 @@ export default class ArrecadacaoDAO{
         });
 
         return municipios;
+    }
+
+    carregarArrecadacoesPorAno(ano) {
+        if (ano in ANOS){
+            let coluna = ANOS[ano];
+            let range = `${coluna}4:${coluna}185`;
+
+            let arrecadacoes = this.extrairDadosDaPlanilha(range);
+            let nomesMunicipios = this.extrairDadosDaPlanilha('A4:A185');
+            console.log(arrecadacoes,nomesMunicipios);
+        }
+    }
+
+    extrairDadosDaPlanilha(range){
+         /**
+         * Aponto para o arquivo 
+         * da planilha de dados,
+         * a partir da pasta atual
+         * caminho relativo, não absoluto
+         */
+        let caminhoArquivo = path.resolve(__dirname,'../../data/dados.xls');
+         
+        let dados = xlsToJson({
+            source: fs.readFileSync(caminhoArquivo),
+            range //range: range
+        });
+
+        return dados;
     }
 }
